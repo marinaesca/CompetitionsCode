d9ada398896f4fe4c8769d3daf84df7fc5f6044e
marinaesca <marinaescalante@utexas.edu>
2017-09-22 19:48:49 -0500

    cleaned up

--- fall17/Shirts.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-shirts = int(raw_input().strip())
-
-for i in range(shirts):
-    line = raw_input()
-    newLine = line[10:-30]
-    print(newLine)
